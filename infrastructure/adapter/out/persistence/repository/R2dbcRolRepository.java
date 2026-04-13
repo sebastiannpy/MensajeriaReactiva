e13ad64e82ea984cf87fbe90248fcbd7c40ba0d3
sebastiannpy <sebastian.certuche01@unicatolica.edu.co>
2026-04-13 18:53:46 -0500

    Delete infrastructure directory

--- infrastructure/adapter/out/persistence/repository/R2dbcRolRepository.java
+++
@@ -1,9 +0,0 @@
-package com.example.chatreactivo.infrastructure.adapter.out.persistence.repository;
-
-import com.example.chatreactivo.infrastructure.adapter.out.persistence.entity.RolEntity;
-import org.springframework.data.repository.reactive.ReactiveCrudRepository;
-
-import java.util.UUID;
-
-public interface R2dbcRolRepository extends ReactiveCrudRepository<RolEntity, UUID> {
-}
