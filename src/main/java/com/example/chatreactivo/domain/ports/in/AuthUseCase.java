4ea2a0a3ea0b4d2c3d7ad8abf61ab46dc9d051fb
sebastiannpy <sebastian.certuche01@unicatolica.edu.co>
2026-04-25 21:49:11 -0500

    Elimin

--- src/main/java/com/example/chatreactivo/domain/ports/in/AuthUseCase.java
+++
@@ -1,8 +0,0 @@
-package com.example.chatreactivo.domain.ports.in;
-
-import com.example.chatreactivo.domain.model.Usuario;
-import reactor.core.publisher.Mono;
-
-public interface AuthUseCase {
-    Mono<Usuario> login(String username, String password);
-}
--- src/main/java/com/example/chatreactivo/infrastructure/adapter/in/web/dto/LoginRequest.java
+++
@@ -1,9 +0,0 @@
-package com.example.chatreactivo.infrastructure.adapter.in.web.dto;
-
-import jakarta.validation.constraints.NotBlank;
-
-public record LoginRequest(
-        @NotBlank String username,
-        @NotBlank String password
-) {
-}
